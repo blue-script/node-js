39889f4d9a9da12c1b44f0ad6456ca9e4841c789
blue-script <alexm92@inbox.ru>
2024-04-16 15:34:12 +0500

    L-3 feat: "delete" and "post" request

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 const products = [{ id: 1, title: 'tomato' }, { id: 2, title: 'orange' }];
 const addresses = [{ id: 1, value: 'Nezalej 12' }, { id: 2, value: 'Selickaga 11' }];
+app.use(express_1.default.json());
 app.get('/products', (req, res) => {
     if (req.query.title) {
         const searchString = req.query.title.toString();
@@ -17,6 +18,14 @@ app.get('/products', (req, res) => {
         res.send(products);
     }
 });
+app.post('/products', (req, res) => {
+    const newProduct = {
+        id: +(new Date()),
+        title: req.body.title
+    };
+    products.push(newProduct);
+    res.status(201).send(newProduct);
+});
 app.get('/products/:id', (req, res) => {
     const product = products.find(p => p.id === +req.params.id);
     if (product) {
@@ -25,6 +34,16 @@ app.get('/products/:id', (req, res) => {
     else {
         res.send(404);
     }
+});
+app.delete('/products/:id', (req, res) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1);
+            res.send(204);
+            return;
+        }
+    }
+    res.send(404);
 });
 app.get('/addresses', (req, res) => {
     res.send(addresses);
